8091fb9f784fe391ce5279d3b7b7335bf0e41737
corbettmax <maxcorbett@trentu.ca>
2023-09-24 13:18:51 -0400

    Polynomials file created and gitignore added

---
+++ assign1/assign1/Polynomials.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Polynomials
+{
+    // A collection of polynomials
+    private List<Polynomial> L;
+    
+    
+    // Creates an empty list L of polynomials
+    //WORK IN PROGRESS I HAVE NO IDEA WHAT IM DOING
+    public Polynomials()
+    { 
+        List<Polynomial> list = new List<Polynomial>();
+        L = list;
+        return;
+    
+    }
+
+    // Retrieves the polynomial stored at position i in L
+    public Polynomial Retrieve(int i) 
+    { 
+
+
+
+
+    }
+
+
+     // Inserts polynomial p into L
+    public void Insert(Polynomial p)
+    { … }
+
+
+    // Deletes the polynomial at index i
+    public void Delete(int i) 
+    { … }
+    
+        
+    // Returns the number of polynomials in L
+    public int Size()
+    { … }
+
+    // Prints out the list of polynomials
+    public void Print()
+    { … }
+
+
+}
